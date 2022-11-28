99eefc59a8c7469540e6ec23c670ecd702e1f088
Naimcoder <isteiaknaim12@gmail.com>
2022-11-28 07:05:21 +0600

    added all items or server

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 8000;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
 const app = express();
 
 // medailewerey
@@ -18,56 +17,65 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
 
 async function run() {
+  // database collection
   const productCollection = client.db("product_Db").collection("BrandName");
   const categoryCollection = client.db("product_Db").collection("allProduct");
   const userCollection = client.db("product_Db").collection("users");
   const bookingsCollection = client.db("product_Db").collection("bookings");
+  const AdvertiseCollection = client.db("product_Db").collection("Advertise");
   try {
-    //
+    //categoryname get
     app.get("/categoryname", async (req, res) => {
       const query = {};
       const result = await productCollection.find(query).toArray();
       res.send(result);
     });
-    //
+
+    //all users post
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    //
+
+    // advertise post
+    app.post("/advertise", async (req, res) => {
+      const addvertise = req.body;
+      const result = await AdvertiseCollection.insertOne(addvertise);
+      res.send(result);
+    });
+
+    // advertise get
+    app.get("/advertise", async (req, res) => {
+      const query = {};
+      const result = await AdvertiseCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //categorys get
     app.get("/categorys", async (req, res) => {
       const query = {};
       const category = await categoryCollection.find(query).toArray();
       res.send(category);
     });
 
-    //get single data
+    //get single categorys
     app.get("/categorys/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { category_id: id };
       const category = await categoryCollection.find(filter).toArray();
       res.send(category);
     });
-    //   app.get('/category/:id', async(req, res) => {
-    //       const id = req.params.id;
-    //       const query = { _id: ObjectId(id)}
-    //       console.log(query)
-    //       const category = await categoryCollection.findOne(query);
-    //       res.send(category);
-    //   })
 
-    //boooking items saved
+    // all boooking post
     app.post("/bookings", async (req, res) => {
       const bookings = req.body;
       const result = await bookingsCollection.insertOne(bookings);
       res.send(result);
     });
-
     //categorys post
     app.post("/categorys", async (req, res) => {
       const caregory = req.body;
@@ -82,7 +90,7 @@ async function run() {
       res.send(users);
     });
 
-    //admin get 
+    //admin get
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
@@ -90,19 +98,7 @@ async function run() {
       res.send({ isAdmin: user?.role === "admin" });
     });
 
-    app.get("/users/seller/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email };
-      const seller = await userCollection.findOne(query);
-      res.send({ isSeller: seller?.role === "seller" });
-    });
-    app.get("/users/user/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email };
-      const user = await userCollection.findOne(query);
-      res.send({ isUser: user?.role === "user" });
-    });
-    //admin role 
+    // admin role
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
@@ -118,16 +114,47 @@ async function run() {
         options
       );
       res.send(result);
-    
     });
-//  admin deleted
-app.delete('/users/admin/:id',async(req,res)=>{
-  const id= req.params.id
-  const query= {_id:ObjectId(id)}
-  const result=await userCollection.deleteOne(query)
-  res.send(result)
-})
-    //all users
+
+    // verifyed patch
+    app.patch("/users/verifyed/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          verifyed: user.verifyed,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+
+    // sing categorys product deleted
+    app.delete("/categorys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await categoryCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+    //  admin deleted
+    app.delete("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+    //all bookings
     app.get("/bookings", async (req, res) => {
       let query = {};
       if (req.query.email) {
@@ -141,6 +168,7 @@ app.delete('/users/admin/:id',async(req,res)=>{
   } finally {
   }
 }
+
 run().catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
